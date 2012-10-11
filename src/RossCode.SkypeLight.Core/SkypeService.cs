8eb819a8e9653e666a63f4be313fc836c20b98d9
RossCode <jross@rosscode.com>
2012-10-11 12:07:40 -0400

    Fix for call checking

--- src/RossCode.SkypeLight.Core/SkypeService.cs
+++ src/RossCode.SkypeLight.Core/SkypeService.cs
@@ -6,38 +6,34 @@ namespace RossCode.SkypeLight.Core
 {
     public class SkypeService
     {
-        private Action setOnCall;
-        private Action setNotOnCall;
         public void Process(Action onCall, Action notOnCall)
         {
-            setOnCall = onCall;
-            setNotOnCall = notOnCall;
-
             var skype = new Skype();
             if (!skype.Client.IsRunning)
             {
                 skype.Client.Start(false, true);
             }
             skype.Attach(6);
-            skype.CallStatus += SkypeOnCallStatus;
-        }
 
-        private readonly IList<TCallStatus> onCallStatuses = new List<TCallStatus>
+            if (skype.ActiveCalls.Count > 0)
             {
-                TCallStatus.clsRinging,
-                TCallStatus.clsInProgress
-            };
-
-        private void SkypeOnCallStatus(Call call, TCallStatus status)
-        {
-            if (onCallStatuses.Contains(status))
-            {
-                setOnCall();
-            } 
+                onCall();
+            }
             else
             {
-                setNotOnCall();
+                notOnCall();
             }
+            skype.CallStatus += (call, status) =>
+                {
+                    if (skype.ActiveCalls.Count > 0)
+                    {
+                        onCall();
+                    }
+                    else
+                    {
+                        notOnCall();
+                    }
+                };
         }
     }
 }
